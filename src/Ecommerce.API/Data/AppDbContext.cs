710e362732d81d7db147ecc1c36a7061302763ca
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-10 12:14:40 +0200

    feature: add AppDbContext.cs

---
+++ src/Ecommerce.API/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using Ecommerce.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Discount> Discounts { get; set; } = null!;
+    public DbSet<Cart> Carts { get; set; } = null!;
+    public DbSet<CartItem> CartItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Role)
+            .HasConversion<string>();
+    }
+}
