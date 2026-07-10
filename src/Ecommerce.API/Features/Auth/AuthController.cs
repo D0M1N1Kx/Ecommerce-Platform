c6c0fb4171102c8773b05f409346aded57449078
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-10 16:26:56 +0200

    feature: add endpoints to AuthController.cs

--- src/Ecommerce.API/Features/Auth/AuthController.cs
+++ src/Ecommerce.API/Features/Auth/AuthController.cs
@@ -1,9 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using LoginRequest = Ecommerce.Shared.DTOs.Auth.Requests.LoginRequest;
+using RegisterRequest = Ecommerce.Shared.DTOs.Auth.Requests.RegisterRequest;
 
 namespace Ecommerce.API.Features.Auth;
 
 [ApiController]
-public class AuthController
+[Route("/auth")]
+public class AuthController : ControllerBase
 {
+    private readonly IAuthService _authService;
     
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        await _authService.RegisterAsync(request.Username, request.Email, request.Password);
+
+        return Ok(new { message = "Registration successful." });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody]LoginRequest request)
+    {
+        var login = await _authService.LoginAsync(request.Email, request.Password);
+        
+        return Ok(new { message = "Login successful.", accessToken = login.AccessToken, refreshToken = login.RefreshToken });
+    }
 }
