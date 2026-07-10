e73280e2809b5904c33db80061e6b3cf9fffa65a
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-10 12:04:19 +0200

    feature: add Cart.cs

---
+++ src/Ecommerce.API/Models/Cart.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Models;
+
+[Table("carts")]
+public class Cart
+{
+    [Key]
+    [Column("id", TypeName = "uuid")]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    [Required]
+    [Column("user_id", TypeName = "uuid")]
+    public Guid UserId { get; set; }
+    
+    [Required]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public User User { get; set; } = null!;
+}
