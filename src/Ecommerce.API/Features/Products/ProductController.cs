7b831b56a97b014f51cf063ffdfe2d3179de1a26
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-15 09:37:39 +0200

    feature: add endpoints to ProductController.cs

--- src/Ecommerce.API/Features/Products/ProductController.cs
+++ src/Ecommerce.API/Features/Products/ProductController.cs
@@ -1,10 +1,55 @@
+using Ecommerce.Shared.DTOs.Products.Requests;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.API.Features.Products;
 
 [ApiController]
-[Route("products")]
+[Route("/products")]
 public class ProductController : ControllerBase
 {
+    private readonly IProductService _productService;
     
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] Guid id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        return Ok(product);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateProductRequest request)
+    {
+        await _productService.CreateAsync(request);
+        return Created();
+    }
+
+    [HttpPatch("{id:guid}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] Guid id, [FromBody] UpdateProductRequest request)
+    {
+        await _productService.UpdateAsync(request);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
+    {
+        await _productService.DeleteAsync(id);
+        return NoContent();
+    }
 }
