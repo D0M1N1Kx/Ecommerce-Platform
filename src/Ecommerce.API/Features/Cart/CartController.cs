8c5d7c8050923cf77ca47e241ee1aa48c63dbdc8
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-16 09:57:19 +0200

    feature: add endpoints to CartController.cs

--- src/Ecommerce.API/Features/Cart/CartController.cs
+++ src/Ecommerce.API/Features/Cart/CartController.cs
@@ -1,9 +1,13 @@
+using System.Security.Claims;
+using Ecommerce.Shared.DTOs.Cart.Requests;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.API.Features.Cart;
 
 [ApiController]
 [Route("/cart")]
+[Authorize(Roles = "Customer,Admin")]
 public class CartController : ControllerBase
 {
     private readonly ICartService _cartService;
@@ -11,5 +15,55 @@ public class CartController : ControllerBase
     public CartController(ICartService cartService)
     {
         _cartService = cartService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCartAsync()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        
+        var cart = await _cartService.GetAsync(userId);
+
+        return Ok(cart);
+    }
+
+    [HttpPost("/items")]
+    public async Task<IActionResult> AddCartItemAsync([FromBody] AddCartItemRequest request)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        await _cartService.AddCartItemAsync(userId, request);
+
+        return Created();
+    }
+
+    [HttpPatch("/items")]
+    public async Task<IActionResult> ChangeQuantityAsync([FromBody] ChangeCartItemQuantityRequest request)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        await _cartService.ChangeQuantityAsync(userId, request);
+
+        return Ok();
+    }
+
+    [HttpDelete("/items/{productId:guid}")]
+    public async Task<IActionResult> DeleteCartItemAsync(Guid productId)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        await _cartService.DeleteCartItemAsync(userId, productId);
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> ClearCart()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        await _cartService.ClearCartAsync(userId);
+
+        return NoContent();
     }
 }
