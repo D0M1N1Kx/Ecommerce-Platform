01b06e2bf9018dbe54853e4187e78a8e4ad0c3c0
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-18 10:44:26 +0200

    feature: add Blazored.LocalStorage and add AuthState.cs with GetAccessTokenAsync and SetTokensAsync functions

---
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Auth/AuthState.cs
@@ -0,0 +1,23 @@
+using Blazored.LocalStorage;
+
+namespace Ecommerce.Blazor.Client.Features.Auth;
+
+public class AuthState
+{
+    private readonly ILocalStorageService _localStorage;
+    private readonly IAuthApiService _authApi;
+
+    public event Action? OnChange;
+    private string? _accessToken;
+
+    public async Task<string?> GetAccessTokenAsync()
+        => _accessToken ??= await _localStorage.GetItemAsStringAsync("accessToken");
+
+    public async Task SetTokensAsync(string access, string refresh)
+    {
+        _accessToken = access;
+        await _localStorage.SetItemAsStringAsync("accessToken", access);
+        await _localStorage.SetItemAsStringAsync("refreshToken", refresh);
+        OnChange?.Invoke();
+    }
+}
