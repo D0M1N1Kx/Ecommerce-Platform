e3568ed3a78afdc8ec0d9411a6e71f6b24406ce4
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-18 10:31:06 +0200

    feature: implement RegisterAsync

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Auth/AuthApiService.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Auth/AuthApiService.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+using Ecommerce.Shared.DTOs.Auth.Requests;
 using Ecommerce.Shared.DTOs.Auth.Responses;
 
 namespace Ecommerce.Blazor.Client.Features.Auth;
@@ -13,7 +15,20 @@ public class AuthApiService : IAuthApiService
     
     public async Task RegisterAsync(string username, string email, string password)
     {
-        throw new NotImplementedException();
+        var request = new RegisterRequest
+        {
+            Username = username,
+            Email = email,
+            Password = password
+        };
+
+        var response = await _http.PostAsJsonAsync("auth/register", request);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException($"Registration failed: {error}");
+        }
     }
 
     public async Task<LoginResponse> LoginAsync(string email, string password)
