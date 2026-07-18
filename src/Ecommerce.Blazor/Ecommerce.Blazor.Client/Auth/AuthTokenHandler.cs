5d0c885af72c945175ff2e6292d1c759834b7cb7
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-18 11:06:38 +0200

    feature: add SendAsync function to AuthTokenHandler.cs

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Auth/AuthTokenHandler.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Auth/AuthTokenHandler.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Ecommerce.Blazor.Client.Features.Auth;
 
 namespace Ecommerce.Blazor.Client.Auth;
@@ -7,4 +8,25 @@ public class AuthTokenHandler : DelegatingHandler
     private readonly AuthState _authState;
     
     public AuthTokenHandler(AuthState authState) => _authState = authState;
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
+    {
+        var token = await _authState.GetAccessTokenAsync();
+        if (!string.IsNullOrEmpty(token))
+            request.Headers.Authorization = new("Bearer", token);
+
+        var response = await base.SendAsync(request, ct);
+
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            var refreshed = await _authState.TryRefreshTokenAsync();
+            if (refreshed)
+            {
+                request.Headers.Authorization = new("Bearer", await _authState.GetAccessTokenAsync());
+                response = await base.SendAsync(request, ct);
+            }
+        }
+
+        return response;
+    }
 }
