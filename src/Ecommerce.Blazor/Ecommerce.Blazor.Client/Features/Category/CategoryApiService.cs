58e717c7531feda749b4aec015cdb9a6be319545
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-20 08:00:30 +0200

    feature: implement GetAllAsync

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Category/CategoryApiService.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Category/CategoryApiService.cs
@@ -1,12 +1,31 @@
+using System.Net.Http.Json;
 using Ecommerce.Shared.DTOs.Category.Responses;
 
 namespace Ecommerce.Blazor.Client.Features.Category;
 
 public class CategoryApiService : ICategoryApiService
 {
+    private readonly HttpClient _http;
+
+    public CategoryApiService(HttpClient http)
+    {
+        _http = http;
+    }
+    
     public async Task<List<CategoryResponse>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var response = await _http.GetAsync("category");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException(error);
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<List<CategoryResponse>>()
+                     ?? throw new InvalidOperationException("Empty response from server");
+
+        return result;
     }
 
     public async Task CreateAsync(string name, string description)
