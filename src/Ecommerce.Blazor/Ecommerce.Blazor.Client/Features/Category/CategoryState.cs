4a7be3e3191cdf35494e8975b70357221a6d7408
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-20 08:12:48 +0200

    feature: add LoadCategoriesAsync and NotifyStateChanged to CategoryState.cs

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Category/CategoryState.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Category/CategoryState.cs
@@ -1,6 +1,24 @@
+using Ecommerce.Shared.DTOs.Category.Responses;
+
 namespace Ecommerce.Blazor.Client.Features.Category;
 
 public class CategoryState
 {
+    private readonly ICategoryApiService _categoryApi;
+    
     public event Action? OnChange;
+    public List<CategoryResponse> Categories { get; private set; } = [];
+
+    public CategoryState(ICategoryApiService categoryApi)
+    {
+        _categoryApi = categoryApi;
+    }
+
+    public async Task LoadCategoriesAsync()
+    {
+        Categories = await _categoryApi.GetAllAsync();
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
 }
