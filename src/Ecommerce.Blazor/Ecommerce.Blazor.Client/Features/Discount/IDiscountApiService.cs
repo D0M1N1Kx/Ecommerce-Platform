54301df08f527b936d8cb4583f1e6ae8b0ae8f68
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-20 08:35:42 +0200

    feature: rename IDiscountService.cs to IDiscountApiService.cs

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Discount/IDiscountApiService.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Discount/IDiscountApiService.cs
@@ -2,7 +2,7 @@ using Ecommerce.Shared.DTOs.Discount.Responses;
 
 namespace Ecommerce.Blazor.Client.Features.Discount;
 
-public interface IDiscountService
+public interface IDiscountApiService
 {
     Task<List<DiscountResponse>> GetAllAsync();
     Task CreateAsync(string name, decimal discountPercentage, DateTime? validUntil);
