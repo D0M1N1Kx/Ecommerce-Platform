58b5d4a6497b4da9ca8cf845f6f4133040383996
D0M1N1Kx <milkovicsd32@gmail.com>
2026-07-22 11:47:12 +0200

    feature: add UpdateAsync

--- src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Product/ProductState.cs
+++ src/Ecommerce.Blazor/Ecommerce.Blazor.Client/Features/Product/ProductState.cs
@@ -8,16 +8,18 @@ public class ProductState
 
     public event Action? OnChange;
     public List<ProductResponse> Products { get; private set; } = [];
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
 
     public ProductState(IProductApiService productApi)
     {
         _productApi = productApi;
     }
 
-    public async Task LoadProductsAsync(string? searchTerm, int? categoryId, decimal? minPrice,
-        decimal? maxPrice, bool? inStockOnly, int page = 1, int pageSize = 10)
+    public async Task LoadProductsAsync(string? searchTerm = null, int? categoryId = null, decimal? minPrice = null,
+        decimal? maxPrice = null, bool? inStockOnly = null)
     {
-        Products = await _productApi.GetAllAsync(page, pageSize, searchTerm, categoryId, minPrice, maxPrice, inStockOnly);
+        Products = await _productApi.GetAllAsync(Page, PageSize, searchTerm, categoryId, minPrice, maxPrice, inStockOnly);
         NotifyStateChanged();
     }
 
@@ -31,7 +33,14 @@ public class ProductState
         int stock, string sku, int categoryId, int? discountId)
     {
         await _productApi.CreateAsync(name, description, price, stock, sku, categoryId, discountId);
-        NotifyStateChanged();
+        await LoadProductsAsync();
+    }
+
+    public async Task UpdateAsync(Guid id, string? name, string? description, decimal? price, int? stock, string? sku,
+        int? categoryId, int? discountId)
+    {
+        await _productApi.UpdateAsync(id, name, description, price, stock, sku, categoryId, discountId);
+        await LoadProductsAsync();
     }
 
     private void NotifyStateChanged() => OnChange?.Invoke();
