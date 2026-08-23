504a364a99817faaf632acc37b2df7005737ee85
D0M1N1K_. <milkovicsd32@gmail.com>
2026-08-23 13:50:38 +0200

    feature: add test RegisterAsync_ValidData_CreatesEmptyCartForUser

--- src/Ecommerce.API.Tests/Cart/CartServiceTests.cs
+++ src/Ecommerce.API.Tests/Cart/CartServiceTests.cs
@@ -1,5 +1,8 @@
 using Ecommerce.API.Data;
+using Ecommerce.API.Features.Auth;
 using Ecommerce.API.Features.Cart;
+using Ecommerce.API.Settings;
+using Ecommerce.API.Shared.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.API.Tests.Cart;
@@ -8,6 +11,7 @@ public class CartServiceTests
 {
     private readonly AppDbContext _db;
     private readonly CartService _cartService;
+    private readonly AuthService _authService;
 
     public CartServiceTests()
     {
@@ -16,7 +20,37 @@ public class CartServiceTests
             .Options;
 
         _db = new AppDbContext(options);
+        
+        var jwtSettings = new JwtSettings
+        {
+            SecretKey = "HE6gOs3hvkrjApAxvQxC3IBawseJEhPDUWWvRyyihD0=",
+            Issuer = "EcommerceAPI",
+            Audience = "EcommerceClient",
+            AccessTokenExpirationMinutes = 15,
+            RefreshTokenExpirationDays = 7
+        };
+
+        var tokenService = new TokenService(jwtSettings);
 
         _cartService = new CartService(_db);
+        _authService = new AuthService(_db, tokenService, jwtSettings);
+    }
+
+    [Fact]
+    public async Task RegisterAsync_ValidData_CreatesEmptyCartForUser()
+    {
+        var username = "testuser";
+        var email = "testuser@ecommerce.com";
+        var password = "VeryStrongPassword";
+
+        await _authService.RegisterAsync(username, email, password);
+
+        var userInDb = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+        Assert.NotNull(userInDb);
+        
+        var cart = await _db.Carts.FirstOrDefaultAsync(c => c.UserId == userInDb.Id);
+
+        Assert.NotNull(cart);
+        Assert.Equal(cart.UserId, userInDb.Id);
     }
 }
