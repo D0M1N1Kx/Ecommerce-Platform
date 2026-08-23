3252ee0997f9292ec332c39fe71450f613f5a03c
D0M1N1K_. <milkovicsd32@gmail.com>
2026-08-23 13:07:56 +0200

    feature: add AuthService and in memory AppDbContext to AuthServiceTests.cs

--- src/Ecommerce.API.Tests/Auth/AuthServiceTests.cs
+++ src/Ecommerce.API.Tests/Auth/AuthServiceTests.cs
@@ -1,6 +1,35 @@
+using Ecommerce.API.Data;
+using Ecommerce.API.Features.Auth;
+using Ecommerce.API.Settings;
+using Ecommerce.API.Shared.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace Ecommerce.API.Tests.Auth;
 
 public class AuthServiceTests
 {
-    
+    private readonly AppDbContext _db;
+    private readonly AuthService _authService;
+
+    public AuthServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _db = new AppDbContext(options);
+
+        var jwtSettings = new JwtSettings
+        {
+            SecretKey = "",
+            Issuer = "EcommerceAPI",
+            Audience = "EcommerceClient",
+            AccessTokenExpirationMinutes = 15,
+            RefreshTokenExpirationDays = 7
+        };
+
+        var tokenService = new TokenService(jwtSettings);
+
+        _authService = new AuthService(_db, tokenService, jwtSettings);
+    }
 }
