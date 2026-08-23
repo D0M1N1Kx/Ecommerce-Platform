ebffb2ccfe7763397fde5be064cb3374ce0a8bb4
D0M1N1K_. <milkovicsd32@gmail.com>
2026-08-23 13:21:18 +0200

    feature: create test RegisterAndLogin_WithValidData_SucceedsAndReturnTokens

--- src/Ecommerce.API.Tests/Auth/AuthServiceTests.cs
+++ src/Ecommerce.API.Tests/Auth/AuthServiceTests.cs
@@ -21,7 +21,7 @@ public class AuthServiceTests
 
         var jwtSettings = new JwtSettings
         {
-            SecretKey = "",
+            SecretKey = "HE6gOs3hvkrjApAxvQxC3IBawseJEhPDUWWvRyyihD0=",
             Issuer = "EcommerceAPI",
             Audience = "EcommerceClient",
             AccessTokenExpirationMinutes = 15,
@@ -31,5 +31,28 @@ public class AuthServiceTests
         var tokenService = new TokenService(jwtSettings);
 
         _authService = new AuthService(_db, tokenService, jwtSettings);
+    }
+
+    [Fact]
+    public async Task RegisterAndLogin_WithValidData_SucceedsAndReturnTokens()
+    {
+        var username = "testuser";
+        var email = "test@ecommerce.com";
+        var password = "VeryStrongPassword";
+
+        await _authService.RegisterAsync(username, email, password);
+
+        var userInDb = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+        Assert.NotNull(userInDb);
+        Assert.Equal(username, userInDb.Username);
+
+        var loginResponse = await _authService.LoginAsync(email, password);
+
+        Assert.NotNull(loginResponse);
+        Assert.False(string.IsNullOrEmpty(loginResponse.AccessToken));
+        Assert.False(string.IsNullOrEmpty(loginResponse.RefreshToken));
+
+        var refreshTokenInDb = await _db.RefreshTokens.FirstOrDefaultAsync(rt => rt.UserId == userInDb.Id);
+        Assert.NotNull(refreshTokenInDb);
     }
 }
