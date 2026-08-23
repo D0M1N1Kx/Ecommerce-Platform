ea1de481cb9cf667fa4cd05621eabc78a1a86675
D0M1N1K_. <milkovicsd32@gmail.com>
2026-08-23 13:39:31 +0200

    feature: add in memory AppDbContext to CartServiceTests

--- src/Ecommerce.API.Tests/Cart/CartServiceTests.cs
+++ src/Ecommerce.API.Tests/Cart/CartServiceTests.cs
@@ -1,6 +1,22 @@
+using Ecommerce.API.Data;
+using Ecommerce.API.Features.Cart;
+using Microsoft.EntityFrameworkCore;
+
 namespace Ecommerce.API.Tests.Cart;
 
 public class CartServiceTests
 {
-    
+    private readonly AppDbContext _db;
+    private readonly CartService _cartService;
+
+    public CartServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _db = new AppDbContext(options);
+
+        _cartService = new CartService(_db);
+    }
 }
